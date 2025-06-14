0eb022dd541c20aa747c1ec20a770afaf4af2542
WestEdgeFamily <westedgefamilydesigns@gmail.com>
2025-06-14 14:56:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const favoritesRoutes = require('./routes/favorites');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', favoritesRoutes);
 
 // Mock database
 let users = [
