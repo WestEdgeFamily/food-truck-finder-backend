65a74b51c7a05230ef102c8761706e4c0df5c2ba
WestEdgeFamily <westedgefamilydesigns@gmail.com>
2025-06-14 15:11:05 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+   customUserId: {
+    type: String,
+    unique: true,
+    required: true
+  },
   email: {
     type: String,
     required: true,
