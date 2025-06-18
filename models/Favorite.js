0eea396692cc9223867f0faf94b5864873033d09
Cody Vincent <westedgefamilydesigns@gmail.com>
2025-06-17 23:03:51 -0600

    Update Favorite.js

--- models/Favorite.js
+++ models/Favorite.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
 const favoriteSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  truckId: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  userId: {
+    type: String,
+    required: true
+  },
+  truckId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-// Ensure a user can't favorite the same truck twice
+// Create compound index to prevent duplicate favorites
 favoriteSchema.index({ userId: 1, truckId: 1 }, { unique: true });
 
-module.exports = mongoose.model('Favorite', favoriteSchema); 
+module.exports = mongoose.model('Favorite', favoriteSchema);
