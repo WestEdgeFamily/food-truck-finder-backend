8732d46dbd86dbaeddf1874b84964197fe7dbb67
Cody Vincent <westedgefamilydesigns@gmail.com>
2025-07-03 15:02:03 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   _id: {
@@ -35,10 +36,69 @@ const userSchema = new mongoose.Schema({
   createdAt: {  // Added createdAt field used in server.js
     type: Date,
     default: Date.now
+  },
+  lastLogin: {
+    type: Date
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  refreshToken: {
+    type: String
   }
   // Removed favorites array - using separate Favorite model instead
   // Removed bcrypt password hashing - using plain text to match server.js
   // Removed complex profile fields not used in server.js
 });
+
+// Index for better query performance
+userSchema.index({ email: 1 });
+userSchema.index({ _id: 1 });
+userSchema.index({ role: 1 });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) return next();
+  
+  try {
+    // Generate salt and hash password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Method to generate JWT payload
+userSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id.toString(),
+    id: this._id.toString(),
+    email: this.email,
+    name: this.name,
+    role: this.role,
+    businessName: this.businessName
+  };
+};
+
+// Remove password from JSON responses
+userSchema.methods.toJSON = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.refreshToken;
+  return obj;
+};
 
 module.exports = mongoose.model('User', userSchema); 
